243baf90c5e88cb5ed41851d9ba964736a94e01b
adikauzd-cloud <adikauzd@gmail.com>
2026-08-11 12:41:07 -0700

    Update adikabot.py

--- adikabot.py
+++ adikabot.py
@@ -978,6 +978,7 @@ async def notify_brokers(bot, message_text: str, req_id: int, buyer_id: int):
     SELLER_SUB,
     SELLER_SUBCAT, 
     SELLER_ACTION, 
+    SELLER_PROPERTY,
     SELLER_CONDITION, 
     SELLER_LOCATION, 
     SELLER_PRICE, 
@@ -988,7 +989,7 @@ async def notify_brokers(bot, message_text: str, req_id: int, buyer_id: int):
     SELLER_CONTACT_VAL, 
     SELLER_PHOTO, 
     SELLER_CONFIRM
-) = range(100, 114)
+) = range(100, 115)
 
 
 # ==============================================================================
